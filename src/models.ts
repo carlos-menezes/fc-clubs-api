43965c76f9b64d69240b8ac17b4af81d020d8aae
Carlos Menezes <carlos-menezes212@hotmail.com>
2023-10-22 23:07:23 +0100

    chore: remove nested models

--- src/models.ts
+++ src/models.ts
@@ -1,55 +1,52 @@
 export interface Club {
-  clubId: string
-  wins: string
-  losses: string
-  ties: string
-  gamesPlayed: string
-  gamesPlayedPlayoff: string
-  goals: string
-  goalsAgainst: string
-  cleanSheets: string
-  points: string
-  reputationtier: string
-  clubInfo: ClubInfo
-  platform: string
-  clubName: string
-  currentDivision: string
+  clubId: string;
+  wins: string;
+  losses: string;
+  ties: string;
+  gamesPlayed: string;
+  gamesPlayedPlayoff: string;
+  goals: string;
+  goalsAgainst: string;
+  cleanSheets: string;
+  points: string;
+  reputationtier: string;
+  clubInfo: {
+    name: string;
+    clubId: number;
+    regionId: number;
+    teamId: number;
+    customKit: {
+      stadName: string;
+      kitId: string;
+      seasonalTeamId: string;
+      seasonalKitId: string;
+      selectedKitType: string;
+      customKitId: string;
+      customAwayKitId: string;
+      customThirdKitId: string;
+      customKeeperKitId: string;
+      kitColor1: string;
+      kitColor2: string;
+      kitColor3: string;
+      kitColor4: string;
+      kitAColor1: string;
+      kitAColor2: string;
+      kitAColor3: string;
+      kitAColor4: string;
+      kitThrdColor1: string;
+      kitThrdColor2: string;
+      kitThrdColor3: string;
+      kitThrdColor4: string;
+      dCustomKit: string;
+      crestColor: string;
+      crestAssetId: string;
+    };
+  };
+  platform: string;
+  clubName: string;
+  currentDivision: string;
 }
 
-export interface ClubInfo {
-  name: string
-  clubId: number
-  regionId: number
-  teamId: number
-  customKit: CustomKit
-}
-
-export interface CustomKit {
-  stadName: string
-  kitId: string
-  seasonalTeamId: string
-  seasonalKitId: string
-  selectedKitType: string
-  customKitId: string
-  customAwayKitId: string
-  customThirdKitId: string
-  customKeeperKitId: string
-  kitColor1: string
-  kitColor2: string
-  kitColor3: string
-  kitColor4: string
-  kitAColor1: string
-  kitAColor2: string
-  kitAColor3: string
-  kitAColor4: string
-  kitThrdColor1: string
-  kitThrdColor2: string
-  kitThrdColor3: string
-  kitThrdColor4: string
-  dCustomKit: string
-  crestColor: string
-  crestAssetId: string
-}
 
 export interface OverallStats {
   clubId: string;
