b766f54c3df824e30335ccdb04a20631827a862f
Carlos Menezes <carlos-menezes212@hotmail.com>
2023-11-04 18:14:23 +0000

    Remove unused _ClubInfo interface from models.ts

--- src/models.ts
+++ src/models.ts
@@ -55,39 +55,6 @@ export type ClubInfo = Record<
   }
 >
 
-export interface _ClubInfo {
-  name: string
-  clubId: number
-  regionId: number
-  teamId: number
-  customKit: {
-    stadName: string
-    kitId: string
-    seasonalTeamId: string
-    seasonalKitId: string
-    selectedKitType: string
-    customKitId: string
-    customAwayKitId: string
-    customThirdKitId: string
-    customKeeperKitId: string
-    kitColor1: string
-    kitColor2: string
-    kitColor3: string
-    kitColor4: string
-    kitAColor1: string
-    kitAColor2: string
-    kitAColor3: string
-    kitAColor4: string
-    kitThrdColor1: string
-    kitThrdColor2: string
-    kitThrdColor3: string
-    kitThrdColor4: string
-    dCustomKit: string
-    crestColor: string
-    crestAssetId: string
-  }
-}
-
 export interface OverallStats {
   clubId: string
   bestDivision: string
